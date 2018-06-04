6257cb4c81be2bc374a8c9c3e163371a80e86879
geekzzz <820760596@qq.com>
2018-06-04 12:29:21 +0800

    图片上传

--- pages/note/note.js
+++ pages/note/note.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    loading: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    that = this
   },
 
   /**
@@ -62,5 +62,11 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  getNote: function()
+  {
+    
   }
+
 })
